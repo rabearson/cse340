86ae39892d4368483a21ae74919cb99689b621b2
Chrysologue <ChrysoBeatz@gmail.com>
2025-08-03 22:52:57 +0300

    Setting port and localhost

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.get("/", (req, res) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500
-const host = process.env.HOST || 'localhost'
+const port = process.env.PORT 
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+  console.log(`app listening on ${host}:${port}`)
 })
