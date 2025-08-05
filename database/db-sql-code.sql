b0069bb580293e8c117bbabd98a0af01106b3904
Chrysologue <ChrysoBeatz@gmail.com>
2025-08-05 08:07:30 +0300

    filling remaining query

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -241,3 +241,16 @@ VALUES   (
     'White',
     5
   );
+
+--Updating description
+UPDATE inventory 
+SET 
+inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+
+--Updating images path
+UPDATE inventory 
+SET 
+inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
