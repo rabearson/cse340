eee42b10a2e3dd01b630ba37fdf39b679d6161bb
Chryso <chrysorx@gmail.com>
2025-08-08 14:57:12 +0000

    processing  registration

--- server.js
+++ server.js
@@ -13,9 +13,36 @@ const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index")
+const pool = require("./database/index")
 const errorRoute = require("./routes/errorRoute")
+const session  = require("express-session")
+const flash = require("connect-flash")
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require("connect-pg-simple")(session))({
+    createTableIfMissing: true,
+    pool
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId'
+}))
+// Express Messages Middleware
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View engine and template
@@ -39,6 +66,9 @@ app.use("/inv", inventoryRoute)
 
 //Error route
 app.use("/", errorRoute)
+
+//Account Route
+app.use("/account", accountRoute)
 
 
 // File Not Found Route - must be last route in list
